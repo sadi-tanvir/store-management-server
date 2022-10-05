bf9f72c23651e699e6947c7b3a79cf136c899e18
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-10-05 16:36:22 +0600

    created brand schema

---
+++ src/models/Brand.ts
@@ -0,0 +1,59 @@
+import { Schema, model, Document } from "mongoose"
+import validator from "validator"
+
+export interface BrandSchemaType extends Document {
+    name: string;
+    description: string;
+    email: string;
+    phone: string;
+    website: string;
+    location: string;
+    products: any[];
+    suppliers: any[];
+    status: string;
+}
+
+const brandSchema = new Schema<BrandSchemaType>({
+    name: {
+        type: String,
+        required: [true, "Brand name is required"],
+        trim: true,
+        maxLength: [100, "Brand name cannot be more than 100 characters"],
+        unique: true,
+        lowercase: true
+    },
+    description: String,
+    email: {
+        type: String,
+        validate: [validator.isEmail, "Please provide a valid email"],
+        lowercase: true
+    },
+    phone: String,
+    website: {
+        type: String,
+        validate: [validator.isURL, "Please enter a valid URL"]
+    },
+    location: String,
+    products: [{
+        type: Schema.Types.ObjectId,
+        ref: "Product"
+    }],
+    suppliers: [{
+        name: String,
+        email: String,
+        phone: String,
+        id: {
+            type: Schema.Types.ObjectId,
+            ref: "Supplier"
+        }
+    }],
+    status: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active"
+    }
+}, { timestamps: true })
+
+
+
+export default model<BrandSchemaType>("Brand", brandSchema)
